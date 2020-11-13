eaf711a6c9975b074467501d5ba09e2f905c8006
422404 <elyanpoujol@gmail.com>
2020-11-13 17:39:55 +0100

    Tweaked build scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,10 @@
 plugins {
     kotlin("jvm") version "1.4.10"
     application
-    distribution
-}
-
-application {
-    mainClass.set("org.cmieux.MainKt")
 }
 
 group = "org.cmieux"
-version = "1.0-SNAPSHOT"
+version = "0.1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -18,4 +13,12 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.antlr:antlr4:4.8")
+}
+
+application {
+    mainClassName = "org.cmieux.MainKt"
+
+    applicationDistribution.from("scripts") {
+        includes += "cc1"
+    }
 }
--- src/main/kotlin/org/cmieux/main.kt
+++ src/main/kotlin/org/cmieux/main.kt
@@ -1,8 +1,12 @@
 package org.cmieux
 
 fun main(vararg args: String) {
-    val inputFile = args[0]
-    val outputFile = args[1]
+    if (args.size != 2) {
+        println("Args <input file> and <output file> are required!")
+    } else {
+        val inputFile = args[0]
+        val outputFile = args[1]
 
-    print("HELLO WORLD! $inputFile $outputFile")
+        print("HELLO WORLD! $inputFile $outputFile")
+    }
 }
